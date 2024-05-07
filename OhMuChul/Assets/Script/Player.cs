cc7deaafb44a2f1fffc32bd14d9c101274097f3a
Kim Minjung <kmj13675@gmail.com>
2024-05-08 00:20:02 +0900

    add infinite tilemap

--- OhMuChul/Assets/Script/Player.cs
+++ OhMuChul/Assets/Script/Player.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float moveSpeed = 10f; // 플레이어의 이동 속도
-    public float jumpForce = 10f; // 플레이어의 점프 힘
+    public float moveSpeed = 12f; // 플레이어의 이동 속도
+    public float jumpForce = 15f; // 플레이어의 점프 힘
     public Transform groundCheck; // 바닥 체크를 위한 오브젝트의 위치
     public LayerMask groundLayer; // 바닥의 레이어
 
     private Rigidbody2D rb;
+    public Vector2 moveInput; // Vector2
     private bool isGrounded;
 
     private void Start()
@@ -18,8 +19,7 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         // 키보드 입력을 통해 플레이어를 움직임
-        float moveInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+        moveInput.x = Input.GetAxisRaw("Horizontal"); // x값만 설정
 
         // 바닥 체크
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
@@ -29,5 +29,11 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        // FixedUpdate에서 rigidbody에 속도를 적용
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
     }
 }
---
+++ OhMuChul/Assets/Script/Reposition.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Area"))
+        {
+            return;
+        }
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 myPos = transform.position;
+        float diffx = Mathf.Abs(playerPos.x - myPos.x);
+        // float diffy = Mathf.Abs(playerPos.y - myPos.y);
+        Debug.Log(playerPos);
+        Debug.Log(myPos);
+        Debug.Log(diffx);
+
+        Player playerScript = GameManager.instance.player.GetComponent<Player>();
+        Vector2 playerDir = playerScript.moveInput;
+        float dirx = playerDir.x < 0 ? -1 : 1;
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if (diffx > 32)
+                {
+                    transform.Translate(Vector3.right * dirx * 64);
+
+                }
+                break;
+            case "Enemy": break;
+
+        }
+    }
+}
